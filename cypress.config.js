0b61f1ce8e7757c87f59b4c02478233cc7ac51d3
Srihari Mogalapalli <sriharim@proximabiz.com>
2025-05-13 22:22:26 +0530

    updated baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000', // Update to your app's URL if needed
+    baseUrl: 'https://dev.leaguelens.proximabiz.com/auth',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
