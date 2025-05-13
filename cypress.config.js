68b2cffe2aa3d33c3dffddcd81c99a71e1b21c24
msrihari9 <98769719+msrihari9@users.noreply.github.com>
2025-05-13 22:57:16 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://dev.leaguelens.proximabiz.com/auth',
+    baseUrl: 'https://example.cypress.io',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
